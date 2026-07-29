c2307ddbe8484cb02b35a6f1636cce106baff668
Aaron The Shadow Wolf <37957871+Aaronek10@users.noreply.github.com>
2026-07-29 12:58:30 +0200

    AaronBot: complete cl_init.lua

--- aaronbot/lua/entities/aaronbot_base/cl_init.lua
+++ aaronbot/lua/entities/aaronbot_base/cl_init.lua
@@ -1,12 +1,22 @@
 include("shared.lua")
 
 function ENT:Initialize()
+	self.m_TaskList = {}
+	self.m_ActiveTasks = {}
+	self.m_TaskCallbacks = {}
+
+	self:SetupTaskList(self.m_TaskList)
+	self:SetupTasks()
 end
 
 function ENT:Draw()
 	self:DrawModel()
+	self:RunTask("Draw")
 end
 
 function ENT:DrawTranslucent()
 	self:Draw()
 end
+
+-- Player control client side will be added with playercontrol files
+include("tasks.lua")
